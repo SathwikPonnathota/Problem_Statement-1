e75ca7821540b0483d431b9724037b67590be244
SathwikPonnathota <46217330+SathwikPonnathota@users.noreply.github.com>
2019-06-19 08:14:52 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,25 +1,25 @@
 import socket
 def Main():
-    host = '127.0.0.1'
-    port = 9998
 
-    sock = socket.socket()
-    sock.connect((host, port))
+    host = '127.0.0.1'
+    port = 9999
+
+    clientsocket = socket.socket()
+    clientsocket.connect((host, port))
 
     message = input("Enter your message :  ")
     msg_encode = message.encode()
-    while message != 'q':
-        sock.send(msg_encode)
+    while message != 'quit':
+        clientsocket.send(msg_encode)
 
         print("the message sent to the server is " + str(message))
-        data = sock.recv(10240)
+        data = clientsocket.recv(10240)
 
         print("the message received from the server is   " + str(data.decode()))
 
         message = input("Enter your message : ")
         msg_encode = message.encode()
-    sock.close()
-
+    clientsocket.close()
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -4,29 +4,30 @@ import os
 def main():
 
     host = '127.0.0.1'
-    port = 9998
+    port = 9999
 
-    sock = socket.socket()
-    sock.bind((host, port))
-    sock.listen(1)
+    serversocket = socket.socket()
+    serversocket.bind((host, port))
+    serversocket.listen(1)
     print("waiting for the clients")
 
-    c, address = sock.accept()
+    clientsocket, address = serversocket.accept()
 
     print("connection from " + str(address))
 
     while True:
-        data = c.recv(10240)
+        data = clientsocket.recv(10240)
 
         if not data:
             break
         print("the information received from the client is  " + str(data.decode()))
-        f=os.popen(str(data.decode()))
-        command=f.read()
-        c.send(command.encode())
+
+        executedcommand=os.popen(str(data.decode()))
+        result=executedcommand.read()
+        clientsocket.send(result.encode())
 
 
-    c.close()
+    clientsocket.close()
 
 if __name__ == '__main__':
     main()
